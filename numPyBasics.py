9cbe49c3e367018b425a579062714e25351a038c
jeffcui03 <jeffcui03@yahoo.com>
2017-04-04 22:32:30 -0400

    update

--- numPyBasics.py
+++ numPyBasics.py
@@ -81,6 +81,80 @@ for train_idx, test_idx in ShuffleSplit(len(X), 100, 0.2):
     
 
 
+import pandas as pd
+import numpy as np
+from pandas_datareader import data
+from math import sqrt
+
+
+#make sure the NYSE.txt file is in the same folder as your python script file
+stocks = pd.read_csv('NYSE.txt',delimiter="\t")
+ 
+#set up our empty list to hold the stock tickers
+stocks_list = []
+ 
+#iterate through the pandas dataframe of tickers and append them to our empty list
+for symbol in stocks['Symbol']:
+    stocks_list.append(symbol)
+    
+len(stocks_list)
+
+stocks_list[:5]
+
+#create empty list to hold our return series DataFrame for each stock
+frames = []
+ 
+for stock in stocks_list:
+    try:
+        #download stock data and place in DataFrame
+        df = data.DataReader(stock, 'yahoo',start='1/1/2000')
+        
+        df['Stdev'] = df['Close'].rolling(window=90).std()
+        
+        #create a column to hold our 20 day moving average
+        df['Moving Average'] = df['Close'].rolling(window=20).mean()
+        
+        #create a column which holds a TRUE value if the gap down from previous day's low to next 
+        #day's open is larger than the 90 day rolling standard deviation
+        df['Criteria1'] = (df['Open'] - df['Low'].shift(1)) < -df['Stdev'] 
+        
+        #create a column that holds a TRUE value if both above criteria are also TRUE
+        df['BUY'] = df['Criteria1'] & df['Criteria2']
+        
+        #calculate daily % return series for stock
+        df['Pct Change'] = (df['Close'] - df['Open']) / df['Open']
+        
+        #create a strategy return series by using the daily stock returns where the trade criteria above are met
+        df['Rets'] = df['Pct Change'][df['BUY'] == True] 
+        
+        #append the strategy return series to our list
+        frames.append(df['Rets'])
+        
+        except:
+            pass
+        
+       #concatenate the individual DataFrames held in our list- and do it along the column axis
+masterFrame = pd.concat(frames,axis=1)
+ 
+#create a column to hold the sum of all the individual daily strategy returns
+masterFrame['Total'] = masterFrame.sum(axis=1)
+ 
+#create a column that hold the count of the number of stocks that were traded each day
+#we minus one from it so that we dont count the "Total" column we added as a trade.
+masterFrame['Count'] = masterFrame.count(axis=1) - 1
+ 
+#create a column that divides the "total" strategy return each day by the number of stocks traded that day to get equally weighted return.
+masterFrame['Return'] = masterFrame['Total'] / masterFrame['Count']
+
+
+masterFrame['Return'].dropna().cumsum().plot()
+
+masterFrame['Return'].dropna().cumsum().plot()
+ 
+
+
+
+
 
 
 
